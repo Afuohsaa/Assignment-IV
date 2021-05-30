af8e556535f597a27c7479f72f7fa2581682851f
Nebaval <Nebaval@DESKTOP-7ENT7QP>
2021-05-30 22:04:26 +0300

    getting user input

--- Assignment IV/src/Prime.java
+++ Assignment IV/src/Prime.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
 
 public class Prime {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int n = scanner.nextInt();
 
 	}
 
