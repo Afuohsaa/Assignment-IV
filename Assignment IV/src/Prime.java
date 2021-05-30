a099ca83e168001caea591769ef3bcebe52439ca
Nebaval <Nebaval@DESKTOP-7ENT7QP>
2021-05-30 23:37:21 +0300

    modifying codes

--- Assignment IV/src/Prime.java
+++ Assignment IV/src/Prime.java
@@ -4,17 +4,20 @@ public class Prime {
 
 	public static void main(String[] args) {
 		
+		
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter a number:");
 		int n = scanner.nextInt();
 		
-		int i = 2;
-		while(i<n) {
-			if(n%i == 0) {
+	if (n<1) 
+		System.out.println("Number must be greater than 1");
+	else if(n>=1)	
+	for (int i = 1; i<=n; i++) {
+			if(n%i==0) {
 				
 			}
 		}
-
+		
 	}
-
+		
 }
