f2361ba493a1bb6b32f735d609055fe9e29677e6
Nebaval <Nebaval@DESKTOP-7ENT7QP>
2021-05-31 21:33:03 +0300

    working on the prime number part

--- Assignment IV/src/Prime.java
+++ Assignment IV/src/Prime.java
@@ -3,28 +3,33 @@ import java.util.Scanner;
 public class Prime {
 
 	public static void main(String[] args) {
-		
-		
+
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter a number:");
 		int n = scanner.nextInt();
-		
-	if (n<1) 
-		System.out.println("Number must be greater than 1");
-	else if(n>=1){	
-	for (int i = 2; i<n; i++) {
-		
-		double c= (double)n/i;
+
 		int t=n-2;
-		//System.out.println(c);
-			if(n%i==0 && c==Math.ceil(c)) {
-				System.out.println(n+" is a composite number "
-						+ "and factors are "+i);
-				
+
+		if (n<1) 
+			System.out.println("Number must be greater than 1");
+		else if(n>=1){	
+			for (int i = 2; i<n; i++) {
+
+				double c= (double)n/i;
+
+				if(n%i==0 && c==Math.ceil(c)) {
+					System.out.println(n+" is a composite number and the factors are "+i);
+
+				}
 			}
+
+			System.out.println("\nWith 1st method number of iteration  is: "+t);
 		}
-		
+		else if(n>=1) {
+
+		}
 	}
-	}
-		
 }
+
+		
+
