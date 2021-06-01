2367194561f38b04283afa480a5d97ffd810fb3d
Nebaval <Nebaval@DESKTOP-7ENT7QP>
2021-06-01 18:24:42 +0300

    the above commites were modified because the codes were not working for all numbers

--- Assignment IV/src/Prime.java
+++ Assignment IV/src/Prime.java
@@ -8,37 +8,31 @@ public class Prime {
 		System.out.println("Enter a number:");
 		int n = scanner.nextInt();
 
-		int t=n-2;
-
+		int t=n-2; //since n is should be greater than 1 and less than itself!!!
+		int i;
+		 int c;
+		boolean flag=false;
 		if (n<1) 
-			System.out.println("Number must be greater than 1");
-		else if(n>=1){	
-			for (int i = 2; i<n; i++) {
+			System.out.println("Number must be greater than 1");	
 
-				double c= (double)n/i;
+		for ( i = 2; i <= n/2; i++) {
+			 c =  n/i;
+				      if (n % i == 0 && c==Math.ceil(c)) {
+				        flag = true;
+				        System.out.println(n+" is a composite number and the factors are "+c);
+				      }
+				    }
 
-				if(n%i==0 && c==Math.ceil(c)) {
-					System.out.println(n+" is a composite number and the factors are "+i);
-
-				}
-				
-			}
-
-				for (int j = 1; j<=n; j++) {
-
-					double x= (double)n/j;
-
-					if(n%j!=0 && x==Math.ceil(x)) {
-						System.out.println(n+" is a prime number and the factors are "+j);
-
-
-					}
-				}
-				System.out.println("\nWith 1st method number of iteration  is: "+t);
+			if (!flag)
+			System.out.println(n + " is a prime number and the factors are 1 and "+n);
+			else
+			//System.out.println(n+" is a composite number and the factors are ");
+			System.out.println("\nWith 1st method number of iteration  is: "+t);
+			
 		}
 		
 	}
-}
+	
 
-		
+
 
