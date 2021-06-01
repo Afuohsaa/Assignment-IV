ab296be83fbf2b3b02c86d6c031d04c9b94d21ae
Nebaval <Nebaval@DESKTOP-7ENT7QP>
2021-06-01 20:59:28 +0300

    final commit

--- Assignment IV/src/Prime.java
+++ Assignment IV/src/Prime.java
@@ -7,28 +7,30 @@ public class Prime {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter a number:");
 		int n = scanner.nextInt();
-
+//to test all numbers from 2 to n-1
 		int t=n-2; //since n is should be greater than 1 and less than itself!!!
+		int x=3; // with primality test, first we check if a number is divided by 2 or 3 then by the other numbers less than root(n)
 		int i;
-		 int c;
-		boolean flag=false;
+		int c;
+		boolean isPrime=false;
 		if (n<1) 
 			System.out.println("Number must be greater than 1");	
 
 		for ( i = 2; i <= n/2; i++) {
 			 c =  n/i;
 				      if (n % i == 0 && c==Math.ceil(c)) {
-				        flag = true;
+				        isPrime = true;
 				        System.out.println(n+" is a composite number and the factors are "+c);
 				      }
 				    }
 
-			if (!flag)
+			if (!isPrime)
 			System.out.println(n + " is a prime number and the factors are 1 and "+n);
 			else
-			//System.out.println(n+" is a composite number and the factors are ");
-			System.out.println("\nWith 1st method number of iteration  is: "+t);
 			
+			System.out.println("With 1st method number of iteration  is: "+t
+					+"\nWith 2nd method number of iteration  is: "+x);
+		
 		}
 		
 	}
